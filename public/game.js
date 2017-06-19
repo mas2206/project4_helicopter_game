e0c88358fa8e075e7fff5d07d072961a883c0f08
Michael Shennan <m.shennan22@gmail.com>
2017-06-19 23:22:23 +0100

    can control player icon using arrows on keyboard

--- public/game.js
+++ public/game.js
@@ -15,6 +15,13 @@ var newCanvas = {
     this.context = this.canvas.getContext("2d");
     document.body.appendChild(this.canvas);
     this.interval = setInterval(updateGame, 30);
+
+    window.addEventListener("keydown", function(event) {
+      newCanvas.key = event.keyCode;
+    })
+    window.addEventListener("keyup", function(event) {
+      newCanvas.key = false;
+    })
   },
 
   clear: function() {
@@ -27,16 +34,46 @@ function shape(width, height, colour, xCoord, yCoord) {
   this.height = height;
   this.xCoord = xCoord;
   this.yCoord = yCoord;
+  this.xSpeed = 0;
+  this.ySpeed = 0;
   this.update = function() {
     ctx = newCanvas.context;
     ctx.fillStyle = colour;
     ctx.fillRect(this.xCoord, this.yCoord, this.width, this.height);
   }
+  this.newPosition = function() {
+    this.xCoord += this.xSpeed;
+    this.yCoord += this.ySpeed;
+  }
 }
 
 function updateGame() {
   newCanvas.clear();
-  // playerIcon.xCoord += 1;
+
+  playerIcon.xSpeed = 0;
+  playerIcon.ySpeed = 0;
+
+  //MOVE LEFT
+  if (newCanvas.key == 37) {
+    playerIcon.xSpeed = -10;
+  }
+
+  //MOVE UP
+  if (newCanvas.key == 38) {
+    playerIcon.ySpeed = -10;
+  }
+
+  //MOVE RIGHT
+  if (newCanvas.key == 39) {
+    playerIcon.xSpeed = 10;
+  }
+
+  //MOVE DOWN
+  if (newCanvas.key == 40) {
+    playerIcon.ySpeed = 10;
+  }
+
+  playerIcon.newPosition();
   playerIcon.update();
 }
 
